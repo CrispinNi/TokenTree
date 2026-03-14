db2a849effcc2ab55ef9a45dde29314a01e544fe
CrispinNi <crispinni013@gmail.com>
2026-03-14 18:20:34 +0200

    fix api url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { logout } from "../utils/auth";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL || "https://fastapi-backend-6rew.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
